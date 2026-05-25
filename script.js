b9e6394daea3c36d04c285585364f897ddd0f75e
souzax007 <marcos@albesbrasil.combr>
2026-05-25 09:48:30 -0300

    adicionado arquivo de usuários

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs").promises;
 const cors = require("cors");
 
 const app = express();
@@ -6,24 +7,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/login",(req,res) =>{
-    const {name,email,password} = req.body;
+app.post("/login", async (req, res) => {
+    const { name, email, password } = req.body;
 
-    if(name === "xxxx" && email === "xxxx" && password === "xxxx"){
-        return res.json({
-            success:true,
-            message: "Login OK",
-            Location:"main.html"
+    try {
+        //ler json
+        const data = await fs.readFile("db/usuarios.json", "utf-8");
+
+        //converter json para array
+        const users = JSON.parse(data);
+
+        const usuario = users.find(user =>
+            user.nome === name &&
+            user.email === email &&
+            user.password === password
+        )
+
+        if (usuario) {
+            return res.json({
+                success: true,
+                message: "Login OK",
+                Location: "main.html"
+            });
+        } else {
+            return res.json({
+                success: false,
+                message: "Erro no login"
+            });
+        };
+    } catch (error) {
+        console.error("Erro ao ler o arquivo:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Erro interno do servidor"
         });
-    }else{
-        return res.json({
-            success:false,
-            message:"Erro no login"
-        });
-    };
-})
+    }
+    });
+    
 
 
-app.listen(9090,function() {
+app.listen(9090, function () {
     console.log("http://localhost:9090");
 });
